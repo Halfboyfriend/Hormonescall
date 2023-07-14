49512d936bcaccc03cda7f832f516f8e9afcff29
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-14 21:57:57 +0100

    Animations

--- js/script.js
+++ js/script.js
@@ -13,3 +13,35 @@ navItemEls.forEach(navItemEl => {
     hamburgerEl.classList.remove('hamburger--open');
   });
 });
+
+// ANIMATIONS
+// window.addEventListener('scroll', function () {
+//   const hidden = document.querySelector('.hidden');
+//   const hiddenHeight = hidden.getBoundingClientRect().top;
+//   const windowHeight = window.innerHeight;
+
+//   console.log(hiddenHeight);
+//   console.log(windowHeight);
+
+//   if (hiddenHeight < windowHeight){
+//     hidden.classList.add('show');
+//   }else {
+//     hidden.classList.remove('show');
+//   }
+// })
+
+
+document.addEventListener('scroll', function() {
+  var sections = document.querySelectorAll('section');
+  
+  sections.forEach(function(section) {
+    var rect = section.getBoundingClientRect();
+    var isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+    
+    if (isVisible) {
+      section.classList.add('section-animation');
+    } else {
+      section.classList.remove('section-animation');
+    }
+  });
+});
